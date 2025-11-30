4d99a4e66e9a18b5cc0d3d3a89ddb48c26e9d5ab
root <root@wien1.exploro.one>
2025-11-30 13:18:37 +0000

    start mocking

--- web/globalping/app/page.tsx
+++ web/globalping/app/page.tsx
@@ -1,13 +1,75 @@
 "use client";
 
-import { Box, Card, Typography, CardContent } from "@mui/material";
+import {
+  Box,
+  Card,
+  Typography,
+  CardContent,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+
+type PingSample = {
+  from: string;
+  target: string;
+
+  // in the unit of milliseconds
+  latencyMs: number;
+}
 
 export default function Home() {
+  const sources = [
+    "lax1",
+    "vie1",
+    "ams1",
+    "dal1",
+    "fra1",
+    "nyc1",
+    "hkg1",
+    "sgp1",
+    "tyo1",
+  ];
+
+  const targets = [
+    "google.com",
+    "github.com",
+    "8.8.8.8",
+    "x.com",
+    "example.com",
+    "1.1.1.1",
+    "223.5.5.5",
+  ];
+
+  const pingSamples: PingSample[] = [];
+
   return (
-    <Box>
+    <Box sx={{ padding: 2 }}>
       <Card>
         <CardContent>
           <Typography variant="h6">Global Ping</Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Target</TableCell>
+                {sources.map((source) => (
+                  <TableCell key={source}>{source}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {targets.map((target) => (
+                <TableRow key={target}>
+                  <TableCell>{target}</TableCell>
+                  {sources.map((source) => (
+                    <TableCell key={source}>{source}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </CardContent>
       </Card>
     </Box>
