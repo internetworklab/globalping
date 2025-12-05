98629fd10813eab662ef22d48b95af2def20a941
root <root@wien1.exploro.one>
2025-12-05 06:06:03 +0000

    fix siso wrapper issue

--- pkg/throttle/common.go
+++ pkg/throttle/common.go
@@ -1,12 +1,11 @@
 package throttle
 
 func WrapWithSISOPipe(origin <-chan interface{}, pipe SISOPipe) <-chan interface{} {
-	outC := pipe.GetOutput()
 	inC := pipe.GetInput()
 	go func() {
 		for pkt := range origin {
 			inC <- pkt
 		}
 	}()
-	return outC
+	return pipe.GetOutput()
 }
