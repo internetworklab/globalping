ce3eb244b9e0dd810dd283e5347bc59e60cbc351
root <root@wien1.exploro.one>
2025-12-18 02:09:29 +0000

    update

--- web/components/traceroutedisplay.tsx
+++ web/components/traceroutedisplay.tsx
@@ -9,12 +9,15 @@ import {
   TableCell,
   TableBody,
   TableContainer,
+  Tab,
+  Tabs,
 } from "@mui/material";
 import { Fragment, useState } from "react";
 import { TaskCloseIconButton } from "@/components/taskclose";
 import { PlayPauseButton } from "./playpause";
 import { getLatencyColor } from "./colorfunc";
 import { IPDisp } from "./ipdisp";
+import { PingSample } from "@/apis/globalping";
 
 type TracerouteIPEntry = {
   ip: string;
@@ -22,6 +25,7 @@ type TracerouteIPEntry = {
 };
 
 type TraceroutePeerEntry = {
+  seq: number;
   ip: TracerouteIPEntry;
   asn?: string;
   location?: string;
@@ -34,6 +38,7 @@ type TracerouteRTTStatsEntry = {
   min: number;
   median: number;
   max: number;
+  history: number[];
 };
 
 type TracerouteStatsEntry = {
@@ -52,6 +57,7 @@ const fakeEntries: TracerouteDispEntry[] = [
   {
     peers: [
       {
+        seq: 3,
         ip: {
           ip: "1.1.1.1",
           rdns: "one.one.one.one",
@@ -61,6 +67,7 @@ const fakeEntries: TracerouteDispEntry[] = [
         isp: "China Telecom",
       },
       {
+        seq: 2,
         ip: {
           ip: "2.2.2.2",
         },
@@ -74,6 +81,7 @@ const fakeEntries: TracerouteDispEntry[] = [
       min: 10.101,
       median: 12.121,
       max: 14.141,
+      history: [10.101, 12.121, 14.141],
     },
     stats: {
       sent: 10,
@@ -83,18 +91,129 @@ const fakeEntries: TracerouteDispEntry[] = [
   },
 ];
 
+type TabState = {
+  maxHop: number;
+  hopEntries: Record<number, TracerouteDispEntry>;
+};
+type PageState = Record<string, TabState>;
+
+function getMedian(history: number[]): number {
+  if (history.length === 0) {
+    return NaN;
+  }
+  if (history.length % 2 === 0) {
+    const lmid_idx = history.length / 2 - 1;
+    const rmid_idx = history.length / 2;
+    return (history[lmid_idx] + history[rmid_idx]) / 2;
+  }
+  const mid_idx = Math.floor(history.length / 2);
+  return history[mid_idx];
+}
+
+function updatePageState(
+  pageState: PageState,
+  pingSample: PingSample
+): PageState {
+  const newState = { ...pageState };
+
+  if (!(pingSample.from in newState)) {
+    newState[pingSample.from] = {
+      maxHop: 1,
+      hopEntries: {},
+    };
+  }
+
+  const newTabState = { ...newState[pingSample.from] };
+  if (pingSample.ttl !== undefined && pingSample.ttl !== null) {
+    if (pingSample.ttl > newTabState.maxHop) {
+      newTabState.maxHop = pingSample.ttl;
+    }
+
+    if (!(pingSample.ttl in newTabState.hopEntries)) {
+      newTabState.hopEntries[pingSample.ttl] = {
+        peers: [],
+        rtts: {
+          current: 0,
+          min: 0,
+          median: 0,
+          max: 0,
+          history: [],
+        },
+        stats: {
+          sent: 0,
+          replied: 0,
+          lost: 0,
+        },
+      };
+    }
+
+    const newEntry = { ...newTabState.hopEntries[pingSample.ttl] };
+    if (pingSample.latencyMs !== undefined && pingSample.latencyMs !== null) {
+      newEntry.rtts.current = pingSample.latencyMs;
+      if (pingSample.latencyMs < newEntry.rtts.min) {
+        newEntry.rtts.min = pingSample.latencyMs;
+      }
+      if (pingSample.latencyMs > newEntry.rtts.max) {
+        newEntry.rtts.max = pingSample.latencyMs;
+      }
+      newEntry.rtts.history = [...newEntry.rtts.history, pingSample.latencyMs];
+      newEntry.rtts.median = getMedian(newEntry.rtts.history);
+      newEntry.stats.replied++;
+    } else {
+      newEntry.stats.lost++;
+    }
+    newEntry.stats.sent++;
+
+    if (pingSample.seq !== undefined && pingSample.seq !== null) {
+      const newPeerEntry: TraceroutePeerEntry = {
+        ip: {
+          ip: pingSample.peer || "",
+          rdns: pingSample.peerRdns,
+        },
+        seq: pingSample.seq,
+        asn: pingSample.peerASN,
+        location: pingSample.peerLocation,
+        isp: pingSample.peerISP,
+      };
+      newEntry.peers = [...newEntry.peers, newPeerEntry];
+      // high seq first
+      newEntry.peers.sort((a, b) => b.seq - a.seq);
+    }
+
+    newTabState.hopEntries[pingSample.ttl] = newEntry;
+  }
+
+  return newState;
+}
+
+type DisplayEntry = {
+  hop: number;
+  entry: TracerouteDispEntry;
+};
+
 export function TracerouteResultDisplay(props: {}) {
-  const [maxHop, setMaxHop] = useState(1);
-  const [hopEntries, setHopEntries] = useState<
-    Record<number, TracerouteDispEntry>
-  >({
-    1: fakeEntries[0],
+  const [hopEntries, setHopEntries] = useState<PageState>({
+    agent1: {
+      maxHop: 1,
+      hopEntries: {
+        1: fakeEntries[0],
+      },
+    },
   });
 
-  const dispEntries: [number, TracerouteDispEntry][] = [];
-  for (let i = 1; i <= maxHop; i++) {
-    if (i in hopEntries) {
-      dispEntries.push([i, hopEntries[i]]);
+  const fakeSources = ["agent1", "agent2", "agent3"];
+  const [tabValue, setTabValue] = useState(fakeSources[0]);
+
+  const dispEntries: DisplayEntry[] = [];
+  const currentTabEntries = hopEntries[tabValue];
+  if (currentTabEntries) {
+    for (let i = 1; i <= currentTabEntries.maxHop; i++) {
+      if (i in currentTabEntries.hopEntries) {
+        dispEntries.push({
+          hop: i,
+          entry: currentTabEntries.hopEntries[i],
+        });
+      }
     }
   }
 
@@ -107,7 +226,17 @@ export function TracerouteResultDisplay(props: {}) {
           alignItems: "center",
         }}
       >
-        <Typography variant="h6">Task #{1}</Typography>
+        <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+          <Typography variant="h6">Task #{1}</Typography>
+          <Tabs
+            value={tabValue}
+            onChange={(event, newValue) => setTabValue(newValue)}
+          >
+            <Tab value={fakeSources[0]} label={fakeSources[0]} />
+            <Tab value={fakeSources[1]} label={fakeSources[1]} />
+            <Tab value={fakeSources[2]} label={fakeSources[2]} />
+          </Tabs>
+        </Box>
         <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
           <PlayPauseButton
             running={true}
@@ -139,7 +268,7 @@ export function TracerouteResultDisplay(props: {}) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {dispEntries.map(([hop, entry]) => {
+            {dispEntries.map(({ hop, entry }) => {
               return (
                 <TableRow key={hop}>
                   <TableCell>{hop}</TableCell>
